aea5c179f9e57b7d238ee5beb0f5e61b4146b85c
AABarabanov <a.a.barabanov91@gmail.com>
2026-03-26 12:59:06 +0400

    инициализация Spring Boot проекта

---
+++ src/main/java/com/aab/dndcalc/DndcalcApplication.java
@@ -0,0 +1,13 @@
+package com.aab.dndcalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DndcalcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DndcalcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aab/dndcalc/DndcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aab.dndcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DndcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
