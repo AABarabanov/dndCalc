04a7f8d2859a0c89496012ae7ba97cbc8187e2c8
AABarabanov <a.a.barabanov91@gmail.com>
2026-03-27 12:34:24 +0400

    внедрен lombok: - добавлена зависимость в pom.xml; - заменены геттеры/сеттеры на Lombok @Getter/@Setter в моделях Hero, Stuff

--- src/main/java/com/aab/dndcalc/model/Hero.java
+++ src/main/java/com/aab/dndcalc/model/Hero.java
@@ -1,5 +1,10 @@
 package com.aab.dndcalc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Hero {
 
     private String name;
@@ -10,59 +15,4 @@ public class Hero {
     private int damageByHero;
     private int loadCapacity;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRace() {
-        return race;
-    }
-
-    public void setRace(String race) {
-        this.race = race;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-
-    public void setLevel(int level) {
-        this.level = level;
-    }
-
-    public int getDefenceClass() {
-        return defenceClass;
-    }
-
-    public void setDefenceClass(int defenceClass) {
-        this.defenceClass = defenceClass;
-    }
-
-    public int getAttackCheck() {
-        return attackCheck;
-    }
-
-    public void setAttackCheck(int attackCheck) {
-        this.attackCheck = attackCheck;
-    }
-
-    public int getDamageByHero() {
-        return damageByHero;
-    }
-
-    public void setDamageByHero(int damageByHero) {
-        this.damageByHero = damageByHero;
-    }
-
-    public int getLoadCapacity() {
-        return loadCapacity;
-    }
-
-    public void setLoadCapacity(int loadCapacity) {
-        this.loadCapacity = loadCapacity;
-    }
 }
--- src/main/java/com/aab/dndcalc/model/Stuff.java
+++ src/main/java/com/aab/dndcalc/model/Stuff.java
@@ -1,7 +1,12 @@
 package com.aab.dndcalc.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 public class Stuff {
 
+    @Getter
+    @Setter
     static class Weapon {
         private String name;
         private String damageType;
@@ -11,129 +16,24 @@ public class Stuff {
         private int weight;
         private boolean inUse;
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getDamageDices() {
-            return damageDices;
-        }
-
-        public void setDamageDices(String damageDices) {
-            this.damageDices = damageDices;
-        }
-
-        public String getDamageType() {
-            return damageType;
-        }
-
-        public void setDamageType(String damageType) {
-            this.damageType = damageType;
-        }
-
-        public int getSkillBonus() {
-            return skillBonus;
-        }
-
-        public void setSkillBonus(int skillBonus) {
-            this.skillBonus = skillBonus;
-        }
-
-        public int getAdditionalModifier() {
-            return additionalModifier;
-        }
-
-        public void setAdditionalModifier(int additionalModifier) {
-            this.additionalModifier = additionalModifier;
-        }
-
-        public int getWeight() {
-            return weight;
-        }
-
-        public void setWeight(int weight) {
-            this.weight = weight;
-        }
-
-        public boolean isInUse() {
-            return inUse;
-        }
-
-        public void setInUse(boolean inUse) {
-            this.inUse = inUse;
-        }
     }
 
+    @Getter
+    @Setter
     static class Shield {
         private int additionalModifier;
         private int weight;
         private int defenceModifier;
         private boolean inUse;
 
-        public int getAdditionalModifier() {
-            return additionalModifier;
-        }
-
-        public void setAdditionalModifier(int additionalModifier) {
-            this.additionalModifier = additionalModifier;
-        }
-
-        public int getWeight() {
-            return weight;
-        }
-
-        public void setWeight(int weight) {
-            this.weight = weight;
-        }
-
-        public int getDefenceModifier() {
-            return defenceModifier;
-        }
-
-        public void setDefenceModifier(int defenceModifier) {
-            this.defenceModifier = defenceModifier;
-        }
-
-        public boolean isInUse() {
-            return inUse;
-        }
-
-        public void setInUse(boolean inUse) {
-            this.inUse = inUse;
-        }
     }
 
+    @Getter
+    @Setter
     static class Other {
         private String name;
         private String description;
         private int weight;
 
-        public String getName() {
-            return name;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void setDescription(String description) {
-            this.description = description;
-        }
-
-        public int getWeight() {
-            return weight;
-        }
-
-        public void setWeight(int weight) {
-            this.weight = weight;
-        }
     }
 }
