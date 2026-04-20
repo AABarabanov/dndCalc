1b4847702b7808187947774af33216bd2fd659e6
AABarabanov <a.a.barabanov91@gmail.com>
2026-04-20 14:52:24 +0400

    метод findByUsername для поиска по имени

--- src/main/java/com/aab/dndcalc/repository/UsersRepository.java
+++ src/main/java/com/aab/dndcalc/repository/UsersRepository.java
@@ -3,5 +3,8 @@ package com.aab.dndcalc.repository;
 import com.aab.dndcalc.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsersRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
